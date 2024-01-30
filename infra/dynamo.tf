2118d69b22d5017ff74bbcd7eab4862f60d366d0
muni <88657097+moonyc@users.noreply.github.com>
2024-01-30 15:51:08 +0100

    Update dynamo.tf to use MonorepoDB dynamoDB table name

--- infra/dynamo.tf
+++ infra/dynamo.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
-  name           = "webdevcody_newsletter"
+  name           = "MonorepoDB"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "pk"
   range_key      = "sk"
